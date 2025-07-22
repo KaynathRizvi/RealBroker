b35d690ea29e7eed87f1c7430c1ddc0ee6f5148e
Kaynath Zehra Rizvi <rizvikaynath@gmail.com>
2025-07-22 16:26:00 +0530

    removed the *

--- server/index.js
+++ server/index.js
@@ -36,9 +36,6 @@ app.get('/', (req, res) => {
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Catch-all to serve React app for any route not handled by API
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
