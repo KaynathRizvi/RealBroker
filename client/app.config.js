67bdb78fbf07c6e3df600f314bdadec0c52af1b3
Kaynath Zehra Rizvi <rizvikaynath@gmail.com>
2025-07-28 16:46:46 +0530

    updated admin

--- client/app.config.js
+++ client/app.config.js
@@ -2,16 +2,16 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: 'YourApp',
-    scheme: 'broker2broker',
+    name: 'RealBroker',
+    scheme: 'realbroker',
     extra: {
       SERVER_URL: process.env.REACT_APP_SERVER_URL,
       ...(process.env.NODE_ENV !== 'production' && {
         DEBUG_SERVER_URL: process.env.REACT_APP_DEBUG_SERVER_URL,
       }),
-      plugins: [
+    },
+    plugins: [
       "expo-secure-store"
     ]
-    },
   },
 };
