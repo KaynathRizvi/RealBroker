7a9e4e32ee71516268881aee1f79a7f7d17e9598
Kaynath Zehra Rizvi <rizvikaynath@gmail.com>
2025-07-28 21:00:36 +0530

    added forgot password in admin

--- server/controllers/passwordController.js
+++ server/controllers/passwordController.js
@@ -14,12 +14,7 @@ const sendResetEmail = async (req, res) => {
     const user = result.rows[0];
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '15m' });
     
-     // ✅ Step: choose base URL based on source
-    const baseUrl = source === 'admin' 
-      ? process.env.ADMIN_URL 
-      : process.env.CLIENT_URL;
-      
-    const resetLink = `${baseUrl}/reset-password?token=${token}`;
+    const resetLink = `${process.env.CLIENT_URL}/reset-password?token=${token}`;
 
     const transporter = nodemailer.createTransport({
       service: 'gmail',
